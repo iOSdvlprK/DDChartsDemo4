3693cceb295d418022cf31ad24fe5cccb72f0783
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-01-07 22:39:28 +0900

    Initial Commit

---
+++ DDChartsDemo4/DDChartsDemo4App.swift
@@ -0,0 +1,17 @@
+//
+//  DDChartsDemo4App.swift
+//  DDChartsDemo4
+//
+//  Created by joe on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct DDChartsDemo4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
